40ffaa1ad363536574e98b91444db182c66a4c36
Anirvesh Arcot <anirvesharcot@Anirveshs-MacBook-Air.local>
2025-07-27 14:43:47 +0530

    changes

--- app/fastapi-backend/core/config.py
+++ app/fastapi-backend/core/config.py
@@ -13,18 +13,27 @@ MONGODB_URI = os.getenv("MONGODB_URI")
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
 ALGORITHM = "HS256"
+CDN_STORAGE_PATH = "./cdn_storage/image_storage"
+FAISS_INDEX_DIR = "./cdn_storage/faiss_indices/"
 
-CDN_STORAGE_PATH = "./cdn_storage/"
-FAISS_INDEX_DIR = "./faiss_indices/"
+def is_mounted(path):
+    """Check if the given path is a mount point"""
+    return os.path.ismount(os.path.abspath(path))
+
+cdn_root = os.path.abspath("./cdn_storage")
+
+if not is_mounted(cdn_root):
+    raise RuntimeError(f"Expected cdn_storage to be mounted at {cdn_root}, but it is not. Mount your FTP first.")
+
+os.makedirs(CDN_STORAGE_PATH, exist_ok=True)
+os.makedirs(FAISS_INDEX_DIR, exist_ok=True)
+
 CACHE_DIR = "./image_cache/"
 ALLOWED_EXTENSIONS = (".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".webp")
 
 SIMILARITY_THRESHOLD=0.5
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-os.makedirs(CDN_STORAGE_PATH, exist_ok=True)
-os.makedirs(FAISS_INDEX_DIR, exist_ok=True)
-
 CURRENT_EVENT_ID = None
 faiss_index = None
 dimension = 4096
