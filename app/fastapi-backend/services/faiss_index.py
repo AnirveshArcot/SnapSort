69729e6900c6ec89593a3eaf1968b69585918943
Anirvesh Arcot <anirvesharcot@Anirveshs-MacBook-Air.local>
2025-07-28 09:08:34 +0530

    removed make dir

--- app/fastapi-backend/services/faiss_index.py
+++ app/fastapi-backend/services/faiss_index.py
@@ -1,19 +1,11 @@
 import os
 import faiss
 
-def is_cdn_mounted(mount_point: str = "./cdn_storage") -> bool:
-    return os.path.ismount(mount_point)
-
 def load_faiss_index(event_id: str, dimension: int, use_id_map: bool = True):
     from core.config import FAISS_INDEX_DIR
 
-    if not is_cdn_mounted():
-        raise RuntimeError("CDN storage is not mounted")
-
     if not event_id:
         raise ValueError("event_id must be provided")
-
-    os.makedirs(FAISS_INDEX_DIR, exist_ok=True)
     path = os.path.join(FAISS_INDEX_DIR, f"{event_id}.faiss")
 
     if os.path.exists(path):
@@ -25,10 +17,5 @@ def load_faiss_index(event_id: str, dimension: int, use_id_map: bool = True):
 
 def save_faiss_index(index: faiss.Index, event_id: str):
     from core.config import FAISS_INDEX_DIR
-
-    if not is_cdn_mounted():
-        raise RuntimeError("CDN storage is not mounted")
-
-    os.makedirs(FAISS_INDEX_DIR, exist_ok=True)
     path = os.path.join(FAISS_INDEX_DIR, f"{event_id}.faiss")
     faiss.write_index(index, path)
